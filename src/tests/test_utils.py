affcb8837807348a689495f12895c1b624f924c2
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-07-19 18:16:15 +0300

    Add dependencies for unit tests and test coverage

---
+++ src/tests/test_utils.py
@@ -0,0 +1,9 @@
+import unittest
+from utils.utils import sieve_of_eratosthenes
+
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_hello_world(self):
+        self.assertEqual("Hello world", "Hello world")
