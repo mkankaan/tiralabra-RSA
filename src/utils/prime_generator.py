6171c47747b737669438777db521bba999efd9a8
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-07-25 20:32:45 +0300

    Create a function for factoring powers of two from a number

--- src/utils/prime_generator.py
+++ src/utils/prime_generator.py
@@ -4,7 +4,23 @@ def generate_prime(bits):
     p = random.getrandbits(bits)
     print("got number with ", bits, " bits: ", p)
     # check primality
+
+    (s, d) = factor_powers_of_two(p)
+    print(s, d)
     return p
+
+def factor_powers_of_two(n):
+    s = 1
+
+    while ((n-1)/pow(2,s)).is_integer():
+        s += 1
+
+    s -= 1
+    d = int((n-1)/pow(2,s))
+
+    return (s, d)
+
+
 
 def generate_two_primes():
     return
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,6 @@
 from math import floor, sqrt
 
+# n = upper bound for the list of primes
 def sieve_of_eratosthenes(n):
     if n < 2:
         return
@@ -8,7 +9,7 @@ def sieve_of_eratosthenes(n):
     primes[0] = False
     primes[1] = False
 
-    for i in range(2, floor(sqrt(n))):  
+    for i in range(2, floor(sqrt(n))):
         if primes[i]:
             j = 0
             multiple = pow(i, 2)+(i*j)
@@ -21,3 +22,12 @@ def sieve_of_eratosthenes(n):
     print(primes)
 
     return primes
+
+
+# n = prime candidate, n > 2
+# k = accuracy
+# return: False (composite) or True (probably prime)
+def miller_rabin(n, k):
+
+    return
+
