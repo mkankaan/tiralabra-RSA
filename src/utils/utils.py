80ab0e67a777879450ad92740a04e9dd78062165
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-07-25 21:39:24 +0300

    Refactor loop condition

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,23 +4,15 @@ from math import floor, sqrt
 def sieve_of_eratosthenes(n):
     if n < 2:
         return
+    
     primes = [True]*(n+1)
-
     primes[0] = False
     primes[1] = False
 
-    for i in range(2, floor(sqrt(n))):
+    for i in range(2, floor(sqrt(n))+1):
         if primes[i]:
-            j = 0
-            multiple = pow(i, 2)+(i*j)
-
-            while multiple <= n:
-                primes[multiple] = False
-                j += 1
-                multiple = pow(i, 2)+(i*j)
-
-    print(primes)
-
+            for j in range(pow(i, 2), n+1, i):
+                primes[j] = False
     return primes
 
 
