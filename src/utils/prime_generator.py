2cc24690960e8f184a3fa802e69d861bb858bc68
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-07-27 20:57:51 +0300

    Finish Miller-Rabin function, clean up code

--- src/utils/prime_generator.py
+++ src/utils/prime_generator.py
@@ -8,14 +8,8 @@ def generate_two_primes():
 
 def generate_prime(bits):
     p = random.getrandbits(bits)
-    print("got number with ", bits, " bits: ", p)
-    # check primality
-
-    p = 7309 # for testing
     (s, d) = factor_powers_of_two(p)
-    print("s, d: ", s, d)
-
-    is_prime(p, s, d)
+    print(p, "is prime?", is_prime(p, s, d))
     return p
 
 
@@ -28,23 +22,20 @@ def factor_powers_of_two(n):
 
     s -= 1
     d = int((n-1)/pow(2,s))
-
-    print(n-1, " = ", "2^", s, "*", d)
     return (s, d)
 
 
 # n = prime candidate
-def is_prime(n, s, d):
+def is_prime(p, s, d):
     MAX_SMALL_PRIME = 4000 # There are approx 500 primes < 4000
     small_primes = small_primes_as_integers(MAX_SMALL_PRIME) #The Sieve of Eratosthenes
 
     # Check if n is divisible by any small prime
-    if is_divisible(n, small_primes):
-        print(n, " is not prime")
+    if is_divisible(p, small_primes):
         return False
 
     k = 40 # Miller-Rabin rounds
-    print(miller_rabin(n, k, s, d))
+    return miller_rabin(p, k, s, d)
 
 
 # n = a prime candidate
@@ -53,7 +44,5 @@ def is_prime(n, s, d):
 def is_divisible(n, list):
     for p in list:
         if n%p == 0:
-            print(n, " is divisible by ", p)
             return True
-    print(n, " is not divisible by any small prime")
     return False
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,9 @@
 from math import floor, sqrt
 from random import randrange
 
+
 # n = upper bound for the list of small primes
+# Returns a boolean list corresponding to the primality of each index
 def sieve_of_eratosthenes(n):
     if n < 2:
         return
@@ -15,53 +17,43 @@ def sieve_of_eratosthenes(n):
             for j in range(pow(i, 2), n+1, i):
                 primes[j] = False
 
-
-    ###### for testing
-    primes_list = []
-    for i in range(len(primes)):
-        if primes[i]:
-            primes_list.append(i)
-    #print("small primes length: ", len(primes_list))
-    ######
-
     return primes
 
 
-# Helper function that takes a boolean list corresponding to primality of index as argument
-# Returns a list of primes up to n as integers
+# Helper function that returns a list of primes up to n as integers
 def small_primes_as_integers(n):
     primes_boolean = sieve_of_eratosthenes(n)
     primes_int = [i for i in range(n) if primes_boolean[i]]
+    print("# of small primes:", len(primes_int))
     return primes_int
 
 
 # n = prime candidate, n > 2
-# k = accuracy (the number of rounds)
+# k = level of accuracy (the number of rounds)
 # Return: False (composite) or True (probably prime)
 def miller_rabin(n, k, s, d):
-    witnesses = set()
-
     # Loop k times
     for i in range(k):
         a = randrange(2, n-1) # Witness
 
-        while a in witnesses:
-            a = randrange(2, n-1)
-        witnesses.add(a)
+        if not test_witness(a, s, d, n):
+            return False
 
-        test_witness(a, s, d, n)
-    
-    #print("witnesses: ", witnesses)
-    
     return True
 
 def test_witness(a, s, d, n):
-    x = pow(a, d)%n
-    print("x: ", x)
+    x = pow(a, d, n) # (a^d) % n
 
-    if abs(x) == 1:
-        return True
+    for i in range(s):
+        y = pow(x, 2, n) # x^2 % n
 
-    # loop s times
-    
-    return
+        if nontrivial_square_root(y, x, n):
+            return False
+        x = y
+
+    # n wasn't proven to be composite, so it's likely prime
+    return True
+
+
+def nontrivial_square_root(y, x, n):
+    return y == 1 and x != 1 and x != n-1
