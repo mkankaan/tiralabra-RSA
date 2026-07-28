c382c9c7c417d5ece38eebbddf1c9bfe6dc823e7
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-07-29 01:31:37 +0300

    Add test cases

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,12 +1,12 @@
 import unittest
-from utils.algorithms import is_divisible, factor_powers_of_two
+from utils.miller_rabin import factor_powers_of_two
 
 class TestUtils(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_factor_powers_of_two_returns_correct_pair(self):
-        numbers = [58303, 82459, 71037, 43337, 85451, 12557, 46609, 681547, 876263, 528897]
+        numbers = [58303, 82459, 71037, 43337, 85451, 12557, 46609, 681547, 876263, 528897, 13, 41]
 
         correct_pairs = [(1, 29151),
                          (1, 41229),
@@ -17,7 +17,9 @@ class TestUtils(unittest.TestCase):
                          (4, 2913),
                          (1, 340773),
                          (1, 438131),
-                         (9, 1033)]
+                         (9, 1033),
+                         (2, 3),
+                         (3, 5)]
 
         for i in range(len(numbers)):
             self.assertEqual(factor_powers_of_two(numbers[i]), correct_pairs[i])
