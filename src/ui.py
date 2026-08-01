33eab03e0cdbb4f2e1617151b03ad86f8b5b175f
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-08-02 00:35:40 +0300

    Create modular multiplicative inverse function

--- src/ui.py
+++ src/ui.py
@@ -1,4 +1,7 @@
 from utils import prime_generator
+from utils.utils import gcd, lcm, mod_inverse
+from key import Key
+
 
 class UI:
     def __init__(self):
@@ -12,9 +15,34 @@ class UI:
 
             match user_input.lower():
                 case "g":
-                    print("generate keys")
-                    BITS = 1024
-                    prime = prime_generator.generate_prime(BITS)
+                    #BITS = 1024
+                    BITS = 16
+                    (p, q) = prime_generator.generate_two_primes(BITS)
+
+                    #print("p:", p)
+                    #print("q:", q)
+
+                    # for testing
+                    p = 39511
+                    q = 5701
+
+                    n = p*q
+
+                    #print("n:", n)
+
+                    g = gcd(p-1, q-1)
+                    print("gcd:", g)
+
+                    l = lcm(p-1, q-1)
+                    print("lcm:", l)
+
+                    e = 65537 # Common public exponent
+
+                    pubic_key = Key(n, e)
+                    print("public key modulus:", pubic_key.modulus, " exp:", pubic_key.exponent)
+
+                    d = mod_inverse(n, l) # mod_inverse(e, lcm)
+                    print("d:", d)
                 case "e":
                     print("encrypt")
                 case "d":
---
+++ src/utils/utils.py
@@ -0,0 +1,53 @@
+# Greatest common divisor
+def gcd(a, b):
+    if b > a:
+        a, b = b, a
+    
+    if b == 0:
+        return a
+
+    return gcd(b, a%b)
+
+
+# Least common multiple
+def lcm(a, b):
+    return int(abs(a*b)/gcd(a, b))
+
+
+# Modular multiplicative inverse, a variant of the extended euclidean algorithm
+# n*x + m*y = gcd(n, m) where gcd(n, m) = 1. Take (mod m) on both sides, giving n*x (mod m) = 1.
+# Return x that satisfies the equation
+def mod_inverse(n, m):
+    # No solution exists if n and m are not coprime
+    if gcd(n, m) != 1:
+        return None
+
+    #if m == 1:
+    #    return 0
+
+    m_0 = m
+    (x, y) = (1, 0)
+
+    while n > 1:
+        print()
+        q = n//m # quotient
+
+        # swap (n, m) -> (m, n%m)
+        t = m # temp
+        m = n%m
+        n = t
+
+
+        # swap (x, y) -> (y, x%y)
+        t = y # temp
+        y = x-q*y
+        x = t
+
+        #(n, m) = (m, n%m)
+        #(x, y) = (y, x-q*y)
+
+    if x < 0:
+        x += m_0
+
+    return x
+
