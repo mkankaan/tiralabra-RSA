727e919880fb8181a0bd9da0cac3656a28708638
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-08-03 02:55:38 +0300

    Add folder for classes

---
+++ src/classes/key.py
@@ -0,0 +1,8 @@
+class Key:
+    def __init__(self, n, e):
+        self.modulus = n
+        self.exponent = e
+
+    # temporary format
+    def __repr__(self):
+     return "-".join((str(self.modulus), str(self.exponent)))
