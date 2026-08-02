65764c09df9811ec33fe29b5083c951f186b0440
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-08-03 02:10:48 +0300

    Move UI code from while loop to separate functions

--- src/ui.py
+++ src/ui.py
@@ -15,79 +15,91 @@ class UI:
 
             match user_input.lower():
                 case "g":
-                    #BITS = 1024
-                    BITS = 16 # for testing
-                    (p, q) = generate_two_primes(BITS)
-
-                    # for testing
-                    #p = 39511
-                    #q = 5701
-
-                    n = p*q
-                    least_common_multiple = lcm(p-1, q-1)
-
-                    e = 65537 # Common public exponent
-                    d = mod_inverse(e, least_common_multiple)
-
-                    public_key = Key(n, e)
-                    private_key = Key(n, d)
-
-                    print("Public key:")
-                    print(public_key)
-                    print()
-                    print("Private key:")
-                    print(private_key)
+                    self.generate_keys()
                 case "e":
-                    (n, e) = (None, None)
-
-                    # will be cleaned up and repetition will be removed
-                    while not n or not e:
-                        public_key = input("Recipient's public key: ")
-                        try:
-                            (n, e) = public_key.split("-")
-                            n = int(n)
-                            e = int(e)
-                        except ValueError:
-                            print("Please give a valid key")
-                            (n, e) = (None, None)
-
-                    plaintext_message = input("Message to encrypt: ")
-                    int_message = int.from_bytes(plaintext_message.encode("utf-8"))
-                    #print("int msg:", int_message)
-
-                    cipher_message = pow(int_message, e, n) # message^d % n
-
-                    print("Encrypted message:")
-                    print(cipher_message)
+                    self.encrypt()
                 case "d":
-                    (n, d) = (None, None)
-                    
-                    while not n or not d:
-                        public_key = input("Recipient's private key: ")
-                        try:
-                            (n, d) = public_key.split("-")
-                            n = int(n)
-                            d = int(d)
-                        except ValueError:
-                            print("Please give a valid key")
-                            (n, d) = (None, None)
-
-                    cipher_message = None
-
-                    while not cipher_message:
-                        cipher_message = input("Message to decrypt: ")
-                        try:
-                            cipher_message = int(cipher_message)
-                        except ValueError:
-                            print("Please give the message as ciphertext")
-                            cipher_message = None
-
-                    int_message = pow(cipher_message, d, n) # cipher^d % n
-                    print("int message:", int_message)
-
-                    bytes = ceil(int_message.bit_length()/8)
-                    plaintext_message = int_message.to_bytes(bytes, byteorder='big').decode("utf-8")
-                    print("Decrypted message:")
-                    print(plaintext_message)
+                    self.decrypt()
                 case _:
                     end = True
+
+
+    def generate_keys(self):
+        #BITS = 1024
+        BITS = 16 # for testing
+        (p, q) = generate_two_primes(BITS)
+
+        # for testing
+        #p = 39511
+        #q = 5701
+
+        n = p*q
+        least_common_multiple = lcm(p-1, q-1)
+
+        e = 65537 # Common public exponent
+        d = mod_inverse(e, least_common_multiple)
+
+        public_key = Key(n, e)
+        private_key = Key(n, d)
+
+        print("Public key:")
+        print(public_key)
+        print()
+        print("Private key:")
+        print(private_key)
+
+
+    def encrypt(self):
+        (n, e) = (None, None)
+        
+        # will be cleaned up and repetition will be removed
+        while not n or not e:
+            public_key = input("Recipient's public key: ")
+            try:
+                (n, e) = public_key.split("-")
+                n = int(n)
+                e = int(e)
+            except ValueError:
+                print("Please give a valid key")
+                (n, e) = (None, None)
+
+        plaintext_message = input("Message to encrypt: ")
+        int_message = int.from_bytes(plaintext_message.encode("utf-8"))
+        #print("int msg:", int_message)
+
+        cipher_message = pow(int_message, e, n) # message^d % n
+
+        print("Encrypted message:")
+        print(cipher_message)
+
+
+    def decrypt(self):
+        (n, d) = (None, None)
+                            
+        while not n or not d:
+            public_key = input("Recipient's private key: ")
+            try:
+                (n, d) = public_key.split("-")
+                n = int(n)
+                d = int(d)
+            except ValueError:
+                print("Please give a valid key")
+                (n, d) = (None, None)
+
+        cipher_message = None
+
+        while not cipher_message:
+            cipher_message = input("Message to decrypt: ")
+            try:
+                cipher_message = int(cipher_message)
+            except ValueError:
+                print("Please give the message as ciphertext")
+                cipher_message = None
+
+        int_message = pow(cipher_message, d, n) # cipher^d % n
+        print("int message:", int_message)
+
+        bytes = ceil(int_message.bit_length()/8)
+        plaintext_message = int_message.to_bytes(bytes, byteorder='big').decode("utf-8")
+        print("Decrypted message:")
+        print(plaintext_message)
