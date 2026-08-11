f7db2cd27ce029fa250f044c749c71344ff0898f
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-08-11 07:36:03 +0300

    Docstring comments

--- src/prime_generator.py
+++ src/prime_generator.py
@@ -3,6 +3,15 @@ from miller_rabin import miller_rabin
 
 
 def generate_prime(bits, small_primes):
+    """Generate a prime number.
+
+    Args:
+        bits (int): Bit length
+        small_primes (list[int]): A list of the smallest prime numbers
+
+    Returns:
+        p (int): A prime number
+    """
     p = getrandbits(bits)
 
     while not is_prime(p, small_primes):
@@ -11,10 +20,19 @@ def generate_prime(bits, small_primes):
     return p
 
 
-# n = prime candidate
-# n = list of small primes
 def is_prime(n, small_primes):
-    # Check if n is divisible by any small prime
+    """Determine if a number is prime.
+    
+    First check divisibility by small primes, if no factors are found
+    then check primality with the Miller-Rabin algorithm.
+
+    Args:
+        n (int): A prime candidate
+        small_primes (list[int]): A list of the smallest prime numbers
+
+    Returns:
+        bool: False if n is composite, True if n is a probable prime
+    """
     for p in small_primes:
         if n%p == 0:
             return False
