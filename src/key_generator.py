fa5c8e92521a34cce6d98737272afcebf5389c8e
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-08-11 06:42:26 +0300

    Docstring comments

--- src/key_generator.py
+++ src/key_generator.py
@@ -10,6 +10,18 @@ PUBLIC_EXPONENT = 65537 # Common public exponent
 
 
 def generate_keys(key_length):
+    """Generate a pair of RSA keys
+
+    Args:
+        key_length (int): The key length in bits
+
+    Returns:
+        (tuple): tuple containing:
+
+            public_key (Key): An RSA public key
+            private_key (Key): An RSA private key 
+    """
+
     small_primes = sieve_of_eratosthenes(MAX_SMALL_PRIME)
 
     p = generate_prime(key_length, small_primes)
@@ -29,19 +41,30 @@ def generate_keys(key_length):
     return (public_key, private_key)
 
 
-# Modular multiplicative inverse, a variant of the extended euclidean algorithm
-# n*x + m*y = gcd(n, m) where gcd(n, m) = 1. Take (mod m) on both sides, giving n*x (mod m) = 1.
-# Return x that satisfies the equation
 def mod_inverse(n, m):
+    """Find a modular multiplicative inverse of n (modulo m)
+
+    Args:
+        n (int): _description_
+        m (int): The modulus
+
+    Returns:
+        x (int): A value that satisfies n*x (mod m) = 1 or None if no solution exists
+    """
+
     # No solution exists if n and m are not coprime
     if gcd(n, m) != 1:
         return None
 
-    m_0 = m # Initial value of the modulus
+    # Store the initial value of the modulus
+    m_0 = m
+
+    # Initial coefficients of x and y in the equation n*x + m*y = gcd(n, m) (Bézout's identity)
     (x, y) = (1, 0)
 
+    # Steps of the extended Euclidean algorithm
     while n > 1:
-        q = n//m # Quotient
+        q = n//m
         (n, m) = (m, n%m)
         (x, y) = (y, x-q*y)
 
