8651375f52c350fd470a772171968ff19c1d0229
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-08-11 20:29:28 +0300

    Move input validation to UI

--- src/encryption_decryption.py
+++ src/encryption_decryption.py
@@ -1,21 +1,11 @@
 def encrypt(n, e, message):
-    if message == "":
-        return ""
-
     # Message encoded into an integer
     m = int.from_bytes(message.encode("utf-8"))
-
-    # Message must have fewer bits than key
-    if m.bit_length() > n.bit_length():
-        return None
     cipher = pow(m, e, n) # m^e % n
     return cipher
 
 
 def decrypt(n, d, cipher):
-    if cipher == "":
-        return ""
-
     int_message = pow(cipher, d, n) # c^d % n
     message_bytes = (int_message.bit_length()+7)//8
     message = int_message.to_bytes(message_bytes).decode("utf-8")
