f5f0f53e9523325b014bf84f2924f090799ad6cb
mkankaan <104016919+mkankaan@users.noreply.github.com>
2026-08-15 17:18:06 +0300

    Add coverage screenshot to testing document, small fixes

--- src/key.py
+++ src/key.py
@@ -1,9 +1,11 @@
-from collections import namedtuple
+from typing import NamedTuple
 
 
-class Key(namedtuple("Key", ["modulus", "exponent"])):
+class Key(NamedTuple):
     """A class to represent an RSA key.
     """
+    modulus: int
+    exponent: int
 
     def __repr__(self): # pragma: no cover
         return "-".join((str(self.modulus), str(self.exponent)))
